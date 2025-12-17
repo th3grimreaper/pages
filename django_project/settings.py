03a68bc2234e1a1cccba0704309aa3652f7e60c7
reaper <saip589@gmail.com>
2025-12-17 09:03:56 +0530

    new updates for deployment

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-rpgw%+no!u0pl)1cd=-==a@(+tzyzlr05znnud5p98!!4-up(x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
